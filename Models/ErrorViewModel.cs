3ca7f5b32723700cdf75e3190cce5723a00eee38
JoÃão Vitor Cezario Sanches <joaovitormanolo@gmail.com>
2024-09-20 15:06:48 -0300

    feat: adds basic ASP.NET Core MVC project structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace vilavelha_case.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
